c4565373cb919f2cecd278fdc64d68274021bb08
RKY-L <bainianl@buffalo.edu>
2023-10-17 11:13:55 -0400

    Division Object

--- calculator/objects/Division.java
+++ calculator/objects/Division.java
@@ -4,6 +4,18 @@ import java.util.*;
 
 public class Division implements Operators{
     public ArrayList<String> calculate(String equation){
+        ArrayList<String> equationSplit = new ArrayList<>(Arrays.asList(equation.split("/")));
+        ArrayList<String> out = new ArrayList<>();
 
+        double total = 0.0;
+
+        total += Double.parseDouble(equationSplit.get(0));
+        total /= Double.parseDouble(equationSplit.get(1));
+
+        out.add(String.valueOf(total));
+
+        out.add("/" + Double.parseDouble(equationSplit.get(1)));
+
+        return out;
     }
 }
